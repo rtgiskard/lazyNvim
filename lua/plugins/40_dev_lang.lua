ae66c4ec0196475146eaf4588ea7daccf61f1133
giskard <rtgiskard@gmail.com>
2024-09-17 23:52:58 +0800

    plugins: venv-selector use regexp branch

--- lua/plugins/40_dev_lang.lua
+++ lua/plugins/40_dev_lang.lua
@@ -4,16 +4,9 @@ return {
 		'linux-cultist/venv-selector.nvim',
 		cmd = { 'VenvSelect', 'VenvSelectCached' },
 		lazy = true,
-		opts = function()
-			local util = require('init.utils')
-
-			return {
-				dap_enabled = util.has('nvim-dap-python'),
-				auto_refresh = false,
-				search = false,
-				search_venv_managers = true,
-				search_workspace = true,
-			}
+		branch = 'regexp',
+		config = function()
+			require('venv-selector').setup()
 		end,
 		dependencies = {
 			'neovim/nvim-lspconfig',
