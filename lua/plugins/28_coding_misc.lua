a22463dbff79f3bf004e0470cb346c0bc45df8f6
giskard <rtgiskard@gmail.com>
2025-03-29 16:43:22 +0800

    plugin: add csvview, gitsigns

--- lua/plugins/28_coding_misc.lua
+++ lua/plugins/28_coding_misc.lua
@@ -54,7 +54,7 @@ return {
 		cmd = { 'TodoTrouble', 'TodoTelescope' },
 		event = { 'BufReadPost', 'BufNewFile' },
 		keys = keymap.todo_comments,
-		config = true,
+		dependencies = { 'nvim-lua/plenary.nvim' },
 	},
 
 	-- d2 diagram
@@ -67,5 +67,32 @@ return {
 	{
 		'sindrets/diffview.nvim',
 		cmd = { 'DiffviewOpen' },
+	},
+
+	-- git buffer integration
+	{
+		'lewis6991/gitsigns.nvim',
+		cmd = { 'Gitsigns' },
+		opts = {},
+	},
+
+	-- csv/tsv
+	{
+		'hat0uma/csvview.nvim',
+		cmd = { 'CsvViewEnable', 'CsvViewDisable', 'CsvViewToggle' },
+		opts = {
+			parser = { comments = { '#', '//' } },
+			keymaps = {
+				-- Text objects for selecting fields
+				textobject_field_inner = { 'if', mode = { 'o', 'x' } },
+				textobject_field_outer = { 'af', mode = { 'o', 'x' } },
+				-- Excel-like navigation: move with tab and enter
+				-- Note: In terminals, you may need to enable CSI-u mode to use <S-Tab> and <S-Enter>.
+				jump_next_field_end = { '<Tab>', mode = { 'n', 'v' } },
+				jump_prev_field_end = { '<S-Tab>', mode = { 'n', 'v' } },
+				jump_next_row = { '<Enter>', mode = { 'n', 'v' } },
+				jump_prev_row = { '<S-Enter>', mode = { 'n', 'v' } },
+			},
+		},
 	},
 }
